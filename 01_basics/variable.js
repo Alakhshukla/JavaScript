17edab6d84f86ff0810acdd4438fec4d49d85a3f
Alakh Shukla <119747626+Alakhshukla@users.noreply.github.com>
2023-09-26 18:12:31 +0000

    changes2

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -4,7 +4,12 @@ let email = "alakh@.com"
 var state = "Uttar Pradesh"
 city = "ghaziabad"   // it is also valid syntax to assign value but we not follow it,
 
-console.table([accId, email, state])
+console.log(email)
+
+// we use console.table to get all value as a table formate
+// console.table([accId, email, state])
+
+
 /* we not use var keyword,
 because block scope and functional scope*/
 let value  // it return undefine

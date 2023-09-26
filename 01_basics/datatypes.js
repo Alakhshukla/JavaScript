744b9fbeb94a9664c90d2452c80130256644ee80
Alakh Shukla <119747626+Alakhshukla@users.noreply.github.com>
2023-09-26 18:46:59 +0000

    Readablity & Datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,34 @@
+"use strict"  // treat all js code as a newer verson
+
+//alert("Hello") // it will not run because we are using nodejs, not browser
+
+/* We've many way to write code.
+but code readability should be high.*/
+console.log(3+8);  console.log("Alakh");
+//it will run bur it's not good practice 
+
+console.log(5+3);  
+console.log("Alakh"); // now it perfect 
+
+let name = "Alakh Shukla" //string
+let age = 19   // number
+let isCorrect = true    //boolean
+
+
+// DATA-TYPES 
+/* 
+number => 2 to power 53
+bigInt => for very big number
+String => ""
+boolean => true/false
+null => standalone value 
+undefine
+symbol => unique(mostly we use in reactjs)
+object => array 
+typeOf
+*/
+
+console.log(typeof(age)) // to know the type of variable
+
+console.log(typeof(null)); // it return object
+console.log(typeof(undefined)); // it return undefine

a5ecd011f028281ccc878c858a875be160664f67
Alakh Shukla <119747626+Alakhshukla@users.noreply.github.com>
2023-10-01 05:54:41 +0000

    Number&Maths

---
+++ math.js
@@ -0,0 +1,17 @@
+const marks = 100
+const score = new Number(18700)
+console.log(marks);
+console.log(score);
+console.log(score.toString().length)
+console.log(score.toFixed(2));
+
+
+/*******************   MATH   *********************/
+
+console.log(Math.abs(-3));  // negetive to positive 
+console.log(Math.abs(3));  // still same
+console.log(Math.round(4.9));
+console.log(Math.round(4.3));
+console.log(Math.max(4,9,87,5,89,2));
+
+console.log(Math.floor(Math.random()*10));
